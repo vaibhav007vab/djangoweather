243a80965d190c218555d224cfdab5f3d3dfb9fb
Vaibhav <vaibhav7996@gmail.com>
2019-09-30 11:32:46 +0530

    initial commit

---
+++ lookupe/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+ path('', views.home, name="home"),
+ ##path('add_city.html', views.add_city, name="add_city"),
+ #path('add_country.html', views.add_country, name="add_country"),
+ path('about.html', views.about, name="about"),
+
+]
